32bee7a7e580113acd33e57bf9e4d5b2191340ef
skyArraon <1312397605@qq.com>
2016-06-17 14:01:06 +0800

    整理JNI

---
+++ sample/src/main/java/com/lalongooo/videocompressor/MainActivity.java
@@ -0,0 +1,164 @@
+package com.lalongooo.videocompressor;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.OpenableColumns;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.yovenny.videocompress.MediaController;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
+public class MainActivity extends Activity {
+
+    private static final int RESULT_CODE_COMPRESS_VIDEO = 3;
+    private static final String TAG = "MainActivity";
+    private EditText editText;
+    private ProgressBar progressBar;
+//    private File tempFile;
+
+    public static final String APP_DIR = "VideoCompressor";
+
+    public static final String COMPRESSED_VIDEOS_DIR = "/Compressed Videos/";
+
+    public static final String TEMP_DIR = "/Temp/";
+
+
+    public static void try2CreateCompressDir() {
+        File f = new File(Environment.getExternalStorageDirectory(), File.separator + APP_DIR);
+        f.mkdirs();
+        f = new File(Environment.getExternalStorageDirectory(), File.separator + APP_DIR + COMPRESSED_VIDEOS_DIR);
+        f.mkdirs();
+        f = new File(Environment.getExternalStorageDirectory(), File.separator + APP_DIR + TEMP_DIR);
+        f.mkdirs();
+    }
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        editText = (EditText) findViewById(R.id.editText);
+
+        findViewById(R.id.btnSelectVideo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("video/*");
+                startActivityForResult(intent, RESULT_CODE_COMPRESS_VIDEO);
+            }
+        });
+
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    protected void onActivityResult(int reqCode, int resCode, Intent data) {
+        if (resCode == Activity.RESULT_OK && data != null) {
+
+            Uri uri = data.getData();
+
+            if (reqCode == RESULT_CODE_COMPRESS_VIDEO) {
+                if (uri != null) {
+                    Cursor cursor = getContentResolver().query(uri, null, null, null, null, null);
+
+                    try {
+                        if (cursor != null && cursor.moveToFirst()) {
+
+                            String displayName = cursor.getString(
+                                    cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                            Log.i(TAG, "Display Name: " + displayName);
+
+                            int sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE);
+                            String size = null;
+                            if (!cursor.isNull(sizeIndex)) {
+                                size = cursor.getString(sizeIndex);
+                            } else {
+                                size = "Unknown";
+                            }
+                            Log.i(TAG, "Size: " + size);
+//                            tempFile = FileUtils.saveTempFile(displayName, this, uri);
+                            editText.setText(uri.getPath());
+
+                        }
+                    } finally {
+                        if (cursor != null) {
+                            cursor.close();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+//    private void deleteTempFile(){
+//        if(tempFile != null && tempFile.exists()){
+//            tempFile.delete();
+//        }
+//    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+//        deleteTempFile();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+//        deleteTempFile();
+    }
+
+    public void compress(View v) {
+//        MediaController.getInstance().scheduleVideoConvert(tempFile.getPath());
+        try2CreateCompressDir();
+        String outPath=Environment.getExternalStorageDirectory()
+                        + File.separator
+                        + APP_DIR
+                        + COMPRESSED_VIDEOS_DIR
+                        +"VIDEO_" + new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date()) + ".mp4";
+        new VideoCompressor().execute(editText.getText().toString(),outPath);
+    }
+
+    class VideoCompressor extends AsyncTask<String, Void, Boolean>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+            Log.d(TAG,"Start video compression");
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            return MediaController.getInstance().convertVideo(params[0],params[1]);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean compressed) {
+            super.onPostExecute(compressed);
+            progressBar.setVisibility(View.GONE);
+            if(compressed){
+                Log.d(TAG,"Compression successfully!");
+            }
+        }
+    }
+
+}
